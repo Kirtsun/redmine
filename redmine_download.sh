0947ab20dbe585d3b035baed4c01bd30c1ad2c23
Oleh Kyrtsun <olegkirtsun@gmail.com>
2023-12-26 22:59:44 +0200

    small changes

--- redmine_download.sh
+++ redmine_download.sh
@@ -2,13 +2,13 @@
 
 redmine_version="5.1.1"
 
-red_url="https://www.redmine.org/releases/redmine-$redmine_version.zip"
 
+curl -o redmine.zip https://www.redmine.org/releases/redmine-$redmine_version.zip && unzip redmine.zip
 
-wget $red_url -P . && unzip redmine-$redmine_version.zip
+rm redmine.zip && mv redmine-$redmine_version redmine
 
-cp ./database.yml ./redmine-5.1.1/config/database.yml
-rm ./redmine-5.1.1/Gemfile
-cp ./Gemfile ./redmine-5.1.1/Gemfile
+cp ./redmine.conf/database.yml ./redmine/config/database.yml
+rm ./redmine/Gemfile
+cp ./redmine.conf/Gemfile ./redmine/Gemfile
 
-echo "Redmine скачан, версия 5.1.1, установлено стандартное подклбчение к базе данных postgresql, если хотите изменить, измените файл database.yml или обратитесь в официальному сайту https://www.redmine.org/projects/redmine/wiki/MySQL_configuration"
+echo "Redmine скачан, версия $redmine_version, установлено стандартное подклбчение к базе данных postgresql, если хотите изменить, измените файл database.yml или обратитесь к официальному сайту https://www.redmine.org/projects/redmine/wiki/MySQL_configuration"
